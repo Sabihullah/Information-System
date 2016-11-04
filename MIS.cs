675b97a2bffb829ee69cb284d5995ada3282c460
Sabihullah <sabihullah.saleh@outlook.com>
2016-11-05 01:14:01 +0500

    Create MIS.cs

---
+++ MIS.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using MySql.Data.MySqlClient;
+using Microsoft.Office.Interop.Excel;
+
+
+public void function()
+{
+
+Console.WriteLine("My name is Sabihullah");
+Console.WriteLine("Roll No"+" i130133");
+}
+
+
+void main(string [] args)
+{
+
+function();
+
+
+}
